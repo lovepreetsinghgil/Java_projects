5f05a4c55042f59be1cf9ee24d541d953eeba1f1
Lovepreet singh gill <48233366+lovepreetsinghgil@users.noreply.github.com>
2024-02-23 21:19:15 +0800

    FIND A PRIME NUMBER WITH LESS ITERATIONS .java

---
+++ prime_number.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in); 
+      int a = sc.nextInt();
+     int count = 0;
+     int n= a-1;
+     int j=a;
+    for(int i=1;i<j;i++)
+    {   
+        if(n%i==0){
+          j=n/i
+            if(i!=j){
+            count+=2;
+            }
+            else{
+            count++;
+            }
+        }
+    }
+    if (count==2){
+  System.out.print("prime number");
+    else
+    System.out.print("none prime number");
+    
+   }
+}
+
+}
