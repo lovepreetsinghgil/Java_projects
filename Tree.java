ffd3f246eb5e58ed76742901bfd4140c7bdbf63e
Lovepreet singh gill <48233366+lovepreetsinghgil@users.noreply.github.com>
2025-04-21 20:01:12 +0800

    trees

---
+++ Tree.java
@@ -0,0 +1,11 @@
+public class Tree {
+    
+ public static int sum(int a,int b){
+    return a+b;
+ }
+ public static void main(String[] args) {
+    int n = 4;
+    int m = 5;
+    System.out.println(sum(n,m));
+ }
+}
